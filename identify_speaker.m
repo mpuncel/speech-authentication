0ec5e18c462b9a54b9d310e8829cc5c0ffb8d4bc
Michael Puncel <mpuncel@mit.edu>
2013-05-13 18:16:36 -0400

    prints top 3

--- identify_speaker.m
+++ identify_speaker.m
@@ -14,20 +14,38 @@ function [ probability_array, speaker_names ] = identify_speaker( path_to_data, 
     
     max_prob = -Inf;
     max_prob_speaker = '';
+    max_prob_2 = -Inf;
+    max_prob_speaker_2 = '';
+    max_prob_3 = -Inf;
+    max_prob_speaker_3 = '';
+    
     for i=1:num_models
         [path, filename, ext] = fileparts(directories{i});
         parts = regexp(path, '/', 'split');
         speaker_name = parts{3};
         probability = speaker_model_probability(path_to_data, directories{i});
+        
         if probability > max_prob
+            max_prob_3 = max_prob_2;
+            max_prob_speaker_3 = max_prob_speaker_2;
+            max_prob_2 = max_prob;
+            max_prob_speaker_2 = max_prob_speaker;
             max_prob = probability;
             max_prob_speaker = speaker_name;
+        elseif probability > max_prob_2
+            max_prob_3 = max_prob_2;
+            max_prob_speaker_3 = max_prob_speaker_2;
+            max_prob_2 = probability;
+            max_prob_speaker_2 = speaker_name;
+        elseif probability > max_prob_3
+            max_prob_3 = probability;
+            max_prob_speaker_3 = speaker_name;   
         end
         probability_array(i) = probability;
         speaker_names{i} = speaker_name;
     end
     outputfile = fopen('classifications.txt', 'a');
-    
-    fprintf(outputfile, '%s\t%s\n', this_speaker, max_prob_speaker);
+
+    fprintf(outputfile, '%s\t%s\t%s\t%s\n', this_speaker, max_prob_speaker, max_prob_speaker_2, max_prob_speaker_3);
     fclose(outputfile);
 end
