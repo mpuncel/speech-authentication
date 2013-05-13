0800e92ed65bd6326de6716cfa2c8646e1aa98f9
Michael Puncel <mpuncel@mit.edu>
2013-05-13 19:05:08 -0400

    statisticsgit checkout -- speaker_model_probability.m

--- identify_speaker.m
+++ identify_speaker.m
@@ -1,8 +1,8 @@
-function [ probability_array, speaker_names ] = identify_speaker( path_to_data, directory_file )
+function identify_speaker( path_to_data, directory_file )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-    [path, filename ext] = fileparts(path_to_data);
+    [path, speech_filename ext] = fileparts(path_to_data);
     parts = regexp(path, '/', 'split');
     this_speaker = parts{3};
     
@@ -21,23 +21,32 @@ function [ probability_array, speaker_names ] = identify_speaker( path_to_data, 
         speaker_name = parts{3};
         probability = speaker_model_probability(path_to_data, directories{i});
       
+        if probability == 0
+            probability
+        end
         if probability > max_prob
             max_prob = probability;
             max_prob_speaker = speaker_name;
+        end
+        
         probability_array(i) = probability;
         speaker_names{i} = speaker_name;
-        end
     end
+    %probability_array
     
     true_probability = speaker_model_probability(path_to_data, strcat('mfcc_data/train_data/', this_speaker, '/gmm8.mat'));
     
-    sorted_probability = sort(probability_array, 'ascend');
-    sorted_probability
+    sorted_probability = sort(probability_array, 'descend');
+    %sorted_probability
     rank = find(sorted_probability==true_probability);
     rank = rank(1);
-    rank
+    %rank
     outputfile = fopen('classifications.txt', 'a');
 
-    fprintf(outputfile, '%s\t%s\t%u\n', this_speaker, max_prob_speaker, rank);
+    %SPEAKER_TESTED SPEAKER_IDENTIFIED RANK_OF_TRUE_SPEAKER
+    %PROBABILITY_OF_TRUE_SPEAKER PROBABILITY_OF_IDENTIFIED_SPEAKER
+    %PROBABILITY_OF_SECOND_CHOICE_SPEAKER AVERAGE_PROBABILITY
+    %PROBABILITY_VARIANCE
+    fprintf(outputfile, '%s\t%s\t%s\t%u\t%f\t%f\t%f\t%f\t%f\n', speech_filename, this_speaker, max_prob_speaker, rank, true_probability, max_prob, sorted_probability(2), mean(sorted_probability), var(sorted_probability));
     fclose(outputfile);
 end
