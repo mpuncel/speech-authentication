64cfa10b9824b4f2c66c924917d9cc8760d32e0c
Michael Puncel <mpuncel@mit.edu>
2013-05-12 22:59:12 -0400

    fixed file i think?

--- train_gaussian.m
+++ train_gaussian.m
@@ -8,21 +8,20 @@ close all
 centroids = dlmread(centroidpath);
 data = dlmread(datapath, '\t');
 numIters = 50;
+centroids = [1; -1];
+data = [2; 1; -1; -2];
 NUM_GAUSSIANS = size(centroids, 1);
-%centroids = [1 -1];
-%data = [2; 1; -1; -2];
 
 [num_frames, DIMENSIONS] = size(data);
 
 weights = zeros(1,NUM_GAUSSIANS) + 1/NUM_GAUSSIANS;
-weights
 variances = cell(1, NUM_GAUSSIANS);
 
 for i=1:NUM_GAUSSIANS
     variances{i} = eye(DIMENSIONS);
 end
 
-for iter = 1:numIters
+for iter = 1:1
     
     denominator = zeros(num_frames, 1);
     prob_gaussian = zeros(NUM_GAUSSIANS, num_frames);
@@ -43,10 +42,10 @@ for iter = 1:numIters
 %     foo = sum(prob_gaussian, 2) * ones(1, 28)
 %    size(foo)
      
-    centroids = prob_gaussian * data ./ (sum(prob_gaussian, 2) * ones(1, 28));
+    centroids = prob_gaussian * data ./ (sum(prob_gaussian, 2) * ones(1, DIMENSIONS));
     
     for i=1:NUM_GAUSSIANS
-        data_minus_centroid = (data - (centroids(i,:); * ones(1, num_frames))').^2;
+        data_minus_centroid = (data - (ones(num_frames, 1) * centroids(i,:))).^2;
         variances{i} = sum(prob_gaussian(i, :)*data_minus_centroid, 1) ./ sum(prob_gaussian(i,:));
     end
 end
@@ -55,4 +54,5 @@ end
 gmm_params.weights = weights;
 gmm_params.means = centroids;
 gmm_params.variances = variances;
-save(strcat(path, 'gmm8.mat'), '-struct', 'gmm_params');
+save(strcat(path, '/gmm8.mat'), '-struct', 'gmm_params');
+path
